c6f75b32f9ce217b427844cd6342ae47f02cf58c
aleesha-wood <spectrat@spectratart.com>
2019-10-02 23:37:39 -0400

    project set up and began added Routes to App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
+import Movie from "./Movies/Movie.js";
+import MovieList from "./Movies/MovieList.js";
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,6 +16,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route to="/" component={MovieList} />
+      <Route to="/movies/:id" component={Movie} />
     </div>
   );
 };
